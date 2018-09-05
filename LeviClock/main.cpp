0053945bd2ed038fd044ed10450b8e7caf390ef1
McBrez <davfreismuth@gmail.com>
2018-09-05 07:10:07 +0200

    Initial Commit

---
+++ LeviClock/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * LeviClock.cpp
+ *
+ * Created: 04.09.2018 07:30:11
+ * Author : davfr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
